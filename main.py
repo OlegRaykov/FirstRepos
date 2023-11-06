62a18ed2bf98da093657eb680a5bdc2f26bfe4cb
Oleg Raykov <johndoe@gmail.com>
2023-11-06 19:51:27 +0300

    add file

---
+++ main.py
@@ -0,0 +1,15 @@
+string = "Some"  # String
+Integer = 7  # Integer
+double = 5.68132  # Float
+bool = False  # Boolean
+
+result = "Результат:"
+
+double -= 5
+x = 8
+res = double % x
+
+num_4 = num_1 = num_2 = 10
+num_3, string2, bool2 = 12, "Word", True
+
+print(result, res, num_3, string2, bool2)
